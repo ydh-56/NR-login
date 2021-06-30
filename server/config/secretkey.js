c447a9d14b5949a0c7784c4ff5f8ffae94892f9f
ydh <govl1535@naver.com>
2021-06-30 09:19:51 +0900

    제외 폴더 적용

--- server/config/secretkey.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    secrectKey :'YoUrSeCrEtKeY',
-    Option:{
-        algorithm : "HS256", // 해싱 알고리즘
-        expiresIn : "30m",  // 토큰 유효 기간
-        issuer : "ydh" // 발행자
-    }
-}
